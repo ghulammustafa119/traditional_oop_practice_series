48a90c95c77f0201c6e27d3074d58c2e6eb82e0f
ghulammustafa119 <ghulammustafabhutto77@gmail.com>
2025-05-05 01:24:30 +0500

    spelling correction of display

--- 14_aggregation.py
+++ 14_aggregation.py
@@ -8,7 +8,7 @@ class Employee():
     def __init__(self, name, emp_id,):
         self.name = name
         self.emp_id = emp_id
-    def desplay_info(self):
+    def display_info(self):
         print(f"Employee Name: {self.name}")
         print(f"Employee Id: {self.emp_id}")
 
@@ -16,13 +16,13 @@ class Department():
     def __init__(self, department_name, employee):
         self.department_name = department_name
         self.employee = employee
-    def desplay_department(self):
+    def display_department(self):
         print(f"Department Name: {self.department_name}")
-        self.employee.desplay_info()
+        self.employee.display_info()
 
 emp1 = Employee("Mustafa",51214)
 dept1 = Department("IT",emp1)
-dept1.desplay_department()
+dept1.display_department()
 del dept1
 print("\nEmployee still exists after Department is deleted:")
-emp1.desplay_info()    
+emp1.display_info()    
